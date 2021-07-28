e262b93c2b726e2e2261f4ac2af2eb46b4750388
vaylon-fernandes <vaylonfernandes@gmail.com>
2021-07-28 08:35:14 +0530

    Add view for download

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from os import environ
 from flask import render_template, redirect, request, flash
 from flask.blueprints import Blueprint
+from flask.helpers import send_file
 
 views = Blueprint("views", __name__)
 
@@ -8,3 +9,9 @@ views = Blueprint("views", __name__)
 @views.route('/', methods=['GET', 'POST'])
 def home():
     return render_template("index.html")
+
+
+@views.route('/download')
+def download():
+    path = "files/Resume-Vaylon-Fernandes.pdf"
+    return send_file(path, as_attachment=True)

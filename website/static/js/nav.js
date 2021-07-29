1a64258beb8fcc2db08b64ef42795ed35614c94a
vaylon-fernandes <vaylonfernandes@gmail.com>
2021-07-29 13:38:07 +0530

    Toggle nav onclick on screen size < 992px

--- website/static/js/nav.js
+++ website/static/js/nav.js
@@ -1,19 +1,37 @@
 const nav = document.getElementById("navWrapper");
 const smallScreen = window.innerWidth < 575;
+const screenMd = window.innerWidth < 992;
 
 window.addEventListener("scroll", fixNav);
 
 function fixNav() {
-  console.log(window.scrollY);
   const verticallyScrolled = window.scrollY > nav.offsetHeight + 75;
 
   if (verticallyScrolled && window.innerWidth > 575) {
     nav.classList.add("scrolled");
+    nav.classList.add("navbar-dark");
+    nav.classList.remove("navbar-light");
   } else {
     nav.classList.remove("scrolled");
+    nav.classList.remove("navbar-dark");
+    nav.classList.add("navbar-light");
   }
 }
 
 if (smallScreen) {
+  window.removeEventListener("scroll", fixNav);
+  nav.classList.remove("navbar-light");
+  nav.classList.add("navbar-dark");
   nav.classList.add("dark-nav");
+}
+const navLinks = document.querySelectorAll(".nav-item");
+const menuToggle = document.getElementById("mainNav");
+const bsCollapse = new bootstrap.Collapse(menuToggle);
+
+if (screenMd) {
+  navLinks.forEach((l) => {
+    l.addEventListener("click", () => {
+      bsCollapse.toggle();
+    });
+  });
 }
